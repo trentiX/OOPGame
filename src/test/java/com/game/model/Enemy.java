4bfab14256a34a4ba256eded655601eaa0eec365
idk <nargizmukanova123@gmail.com>
2026-01-18 18:45:28 +0500

    enemy, weapon classes and their repositories realization

--- src/test/java/com/game/model/Enemy.java
+++ src/test/java/com/game/model/Enemy.java
@@ -8,4 +8,19 @@ public class Enemy {
     private int damage;
     private int expReward;
 
+    public Enemy(int id, String name, String type, int hp, int damage, int expReward) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.hp = hp;
+        this.damage = damage;
+        this.expReward = expReward;
+    }
+
+    public String getName() { return name; }
+    public String getType() { return type; }
+    public int getHp() { return hp; }
+    public void setHp(int hp) { this.hp = hp; }
+    public int getDamage() { return damage; }
+    public int getExpReward() { return expReward; }
 }
--- src/test/java/com/game/repository/WeaponRepository.java
+++ src/test/java/com/game/repository/WeaponRepository.java
@@ -8,6 +8,15 @@ public class WeaponRepository {
     public WeaponRepository(Connection conn) { this.connection = conn; }
 
     public Weapon getWeaponById(int id) throws SQLException {
-
+        String sql = "SELECT * FROM weapons WHERE id = ?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new Weapon(rs.getInt("id"), rs.getString("name"),
+                        rs.getString("type"), rs.getInt("additional_damage"));
+            }
+        }
+        return null;
     }
 }
